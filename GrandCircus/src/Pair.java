a9aab762c362a083c7e3501cdac4001743b87ac5
Tyler Hensley <Tyler Hensley>
2016-06-08 14:49:02 -0400

    2nd commit

--- GrandCircus/src/Pair.java
+++ GrandCircus/src/Pair.java
@@ -1,4 +1,29 @@
+import java.util.Scanner;
 
 public class Pair {
-
+	public static void main(String[] args){
+		System.out.println("Learn your squares and cubes!");
+		
+		int number = 0;
+		int squared = number*number;
+		int cubed = number * number * number;
+		
+		
+		System.out.println("Number" + "\t"  +"Squared " + "\t" + "cubed");
+		
+		do{
+			
+			Scanner scan = new Scanner (System.in);
+			
+			System.out.print("Enter an integer: ");
+			
+			number = scan.nextInt();
+			
+			System.out.println(number + "\t" + squared + "\t" + cubed  );
+			
+			
+			
+			
+		}while(true);
+	}
 }
